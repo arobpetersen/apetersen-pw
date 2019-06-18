3a787ae5e791e69defa45f77d3ad9677aeda3f64
Andrew Petersen <arobpetersen@gmail.com>
2019-06-18 16:07:03 -0400

    final push 6/18

--- src/App.js
+++ src/App.js
@@ -21,9 +21,23 @@ function App() {
         <p>How is the Header Designed?</p>
       </body>
       <footer className="App-footer">
+        <a
+        href="https://github.com/arobpetersen"
+        target="_blank"
+        rel="noopener noreferrer" 
+        >
         <img src={github} className="App-github-footer" alt="logo-github" />
+        </a>
+
         <img src={logo} className="App-logo-footer" alt="logo-react" />
+        
+        <a
+        href="https://www.linkedin.com/in/andrew-petersen-1a4767188"
+        target="_blank"
+        rel="noopener noreferrer" 
+        >
         <img src={linkedin} className="App-linkedin-footer" alt="logo-linkedin"/>
+        </a>
         </footer>
     </div>
   );
