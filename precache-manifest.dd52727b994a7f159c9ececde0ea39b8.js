1d9f90328dd686f15471f60d9a6bfe641b047783
Andrew Petersen <arobpetersen@gmail.com>
2019-06-18 14:04:58 -0400

    Updates

---
+++ precache-manifest.dd52727b994a7f159c9ececde0ea39b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb0bb4f180516128f775d4a547b10f7e",
+    "url": "/apetersen-pw/index.html"
+  },
+  {
+    "revision": "8e44c8cc8f1937649e2c",
+    "url": "/apetersen-pw/static/css/main.cebeb8b8.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/apetersen-pw/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "8e44c8cc8f1937649e2c",
+    "url": "/apetersen-pw/static/js/main.fdb4a247.chunk.js"
+  },
+  {
+    "revision": "e722fb37a1196931adee",
+    "url": "/apetersen-pw/static/js/runtime~main.173900cf.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/apetersen-pw/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/apetersen-pw/precache-manifest.b07315f0294755dd0f142c5569468b20.js"
+  "/apetersen-pw/precache-manifest.dd52727b994a7f159c9ececde0ea39b8.js"
 );
 
 self.addEventListener('message', (event) => {
