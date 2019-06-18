f4b4419e2fc0d8ddb70c247433727330ca2901a3
Andrew Petersen <arobpetersen@gmail.com>
2019-06-18 15:34:29 -0400

    footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './img/logo.svg';
+import github from './img/github.png';
+import linkedin from './img/linkedin.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          React app is working
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React
         </a>
       </header>
+      <body>
+        <p>How is the Header Designed?</p>
+      </body>
+      <footer className="App-footer">
+        <img src={github} className="App-github-footer" alt="logo-github" />
+        <img src={logo} className="App-logo-footer" alt="logo-react" />
+        <img src={linkedin} className="App-linkedin-footer" alt="logo-linkedin"/>
+        </footer>
     </div>
   );
 }
