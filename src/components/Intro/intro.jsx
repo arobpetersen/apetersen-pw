5e508e5da3e4578ad0cea18a100b0fcd2cb149eb
Andrew Petersen <arobpetersen@gmail.com>
2019-07-08 16:16:57 -0400

    margin change

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -7,7 +7,14 @@ export class Intro extends React.Component {
             <div className="intro">
                 <h1>Welcome!</h1>
                 <p>
-                    My name is Andrew Petersen, and I am a Software Engineer.
+                    My name is Andrew Petersen, and I am a Software Engineer. 
+                    I am origionally from Cleveland, Ohio but now I am living in Phoenix, Arizona.
+                    My name is Andrew Petersen, and I am a Software Engineer. 
+                    I am origionally from Cleveland, Ohio but now I am living in Phoenix, Arizona.
+                    My name is Andrew Petersen, and I am a Software Engineer. 
+                    I am origionally from Cleveland, Ohio but now I am living in Phoenix, Arizona.
+                    My name is Andrew Petersen, and I am a Software Engineer. 
+                    I am origionally from Cleveland, Ohio but now I am living in Phoenix, Arizona.
                 </p>
             </div>
         );
