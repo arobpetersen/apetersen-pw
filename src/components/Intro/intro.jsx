2972834a06268cd9e4986acf348d818decc3c123
Andrew Petersen <arobpetersen@gmail.com>
2019-07-16 13:36:25 -0400

    scroll adjustment

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -12,12 +12,12 @@ export class Intro extends React.Component {
                     <h2 id="name">My name is <h2 id="ap">Andrew Petersen</h2><br /><br />and I am a&nbsp;
                         <TextLoop interval={2000} className="textscroll" springConfig={{ stiffness: 30, damping: 10 }}>
                             <span>Software Engineer.</span>
-                            <span>S+ Tier Set, Hun Batz, & Ratatoskr.</span>
                             <span>Climber.</span>
                             <span>Gamer.</span>
                             <span>Student.</span>
+                            <span>Outdoorsman.</span>
                             <span>Leader.</span>
-                        </TextLoop></h2><br /><br /><br />
+                        </TextLoop></h2>
                     <p className="para">
                         I am currently attending and will be graduating from the University of Cincinnati
                         in the Spring of 2020 with a degree in Software Engineering. I grew up in Cleveland, OH
