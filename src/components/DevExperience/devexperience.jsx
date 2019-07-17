767f7adcc19e6142b81ec0a7d88e6bd4618d85d6
Andrew Petersen <arobpetersen@gmail.com>
2019-07-17 15:01:22 -0400

    added test component & typescript to devxp

--- src/components/DevExperience/devexperience.jsx
+++ src/components/DevExperience/devexperience.jsx
@@ -12,6 +12,7 @@ import sql from './imgdev/sql.png';
 import node from './imgdev/node.png';
 import spring from './imgdev/spring.png';
 import kotlin from './imgdev/kotlin.png';
+import typescript from './imgdev/typescript.svg';
 
 export class DevExperience extends React.Component {
     render() {
@@ -23,13 +24,14 @@ export class DevExperience extends React.Component {
                     <img src={javascript} id="js" alt="JavaScript" title="JavaScript" />
                     <img src={html} id="html" alt="HTML" title="HTML" />
                     <img src={css} id="css" alt="CSS" title="CSS" />
-                    <img src={angular} id="angular" alt="AngularJS" title="AngularJS" />
+                    <img src={angular} id="angular" alt="Angular" title="Angular" />
                     <img src={csharp} id="csharp" alt="C#" title="C#" />
                     <img src={java} id="java" alt="Java" title="Java" />
                     <img src={sql} id="sql" alt="SQL" title="SQL" />
                     <img src={node} id="node" alt="NodeJS" title="NodeJS" />
                     <img src={spring} id="spring" alt="Spring Boot" title="Spring Boot" />
                     <img src={kotlin} id="spring" alt="Kotlin" title="Kotlin" />
+                    <img src={typescript} id="typescript" alt="TypeScript" title="TypeScript" />
                 </div>
             </div >
         );
---
+++ src/components/test/test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './test.css';
+
+export class Test extends React.Component {
+    render() {
+        return(
+            <div></div>
+        );
+    }
+}
