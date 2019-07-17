423bc423bb18e12344e5a8998ba6606f5d9fe0fa
Andrew Petersen <arobpetersen@gmail.com>
2019-07-17 15:39:36 -0400

    Updates

---
+++ precache-manifest.4baee996164ed1ce160e4f550463f51f.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "786197f1c36cfc4c1af39c31c10d008b",
+    "url": "/apetersen/index.html"
+  },
+  {
+    "revision": "c6b3fbf8283673ede030",
+    "url": "/apetersen/static/css/main.e102de8e.chunk.css"
+  },
+  {
+    "revision": "5a6ce678285bc2329fe5",
+    "url": "/apetersen/static/js/2.b7224140.chunk.js"
+  },
+  {
+    "revision": "c6b3fbf8283673ede030",
+    "url": "/apetersen/static/js/main.0e329dd4.chunk.js"
+  },
+  {
+    "revision": "9a4aca084a7cba71408b",
+    "url": "/apetersen/static/js/runtime~main.bac8d25a.js"
+  },
+  {
+    "revision": "0711fbfc297d7a62f7e4c504d81852a3",
+    "url": "/apetersen/static/media/accenture.0711fbfc.png"
+  },
+  {
+    "revision": "4c732369e54ee14c3d884b15b441ad0f",
+    "url": "/apetersen/static/media/angular.4c732369.svg"
+  },
+  {
+    "revision": "e8460f0d8b9df01e9460bfd1ade0e8c8",
+    "url": "/apetersen/static/media/csharp.e8460f0d.png"
+  },
+  {
+    "revision": "9fe7584b6d29bec74a66024829929697",
+    "url": "/apetersen/static/media/css.9fe7584b.png"
+  },
+  {
+    "revision": "be3c89fd6dfd4e625ca4a3e50e84a745",
+    "url": "/apetersen/static/media/email.be3c89fd.png"
+  },
+  {
+    "revision": "6d9be9b0b692b55e90eab32fa1440859",
+    "url": "/apetersen/static/media/github.6d9be9b0.png"
+  },
+  {
+    "revision": "4bd53d37d025a0f4f0b40715c4af44fb",
+    "url": "/apetersen/static/media/headlogo.4bd53d37.png"
+  },
+  {
+    "revision": "a7f383f5556a80bfeff63bd43ecdfb68",
+    "url": "/apetersen/static/media/honeywell.a7f383f5.png"
+  },
+  {
+    "revision": "74e943d82452f81f6a0bb0b51ac6785d",
+    "url": "/apetersen/static/media/java.74e943d8.png"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "/apetersen/static/media/javascript.0cded3a3.png"
+  },
+  {
+    "revision": "61ab98abc63e38543277787c3e85a38c",
+    "url": "/apetersen/static/media/kotlin.61ab98ab.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/apetersen/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "38c3a911493bf29320caa2cb901e0ecb",
+    "url": "/apetersen/static/media/node.38c3a911.png"
+  },
+  {
+    "revision": "d7de2b58445460030439bcdfff63e28d",
+    "url": "/apetersen/static/media/phone.d7de2b58.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/apetersen/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "8b69ea43edd18c2676d8633903c0152b",
+    "url": "/apetersen/static/media/reactrouter.8b69ea43.png"
+  },
+  {
+    "revision": "04142af32085c5aaa77b2a4f3e9c7627",
+    "url": "/apetersen/static/media/spring.04142af3.png"
+  },
+  {
+    "revision": "eb745847580c5f20d6c979509f726472",
+    "url": "/apetersen/static/media/sql.eb745847.png"
+  },
+  {
+    "revision": "3de182d2120823fe49c27eeb1765c53a",
+    "url": "/apetersen/static/media/typescript.3de182d2.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.41abeff012bded1149ced9f2f5e858a8.js"
+  "/apetersen/precache-manifest.4baee996164ed1ce160e4f550463f51f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/apetersen/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
