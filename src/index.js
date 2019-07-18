903434424911711610c4ef1abc212ab0bbb43d32
Andrew Petersen <arobpetersen@gmail.com>
2019-07-18 10:11:09 -0400

    routing bug fix

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import * as serviceWorker from './serviceWorker';
 
 const routing = (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
         <div className="NavBar">
             <ul className="header">
                 <li><NavLink to="/ContactApp" activeStyle={{
