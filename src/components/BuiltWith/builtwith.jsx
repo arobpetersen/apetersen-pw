06fe1a885f5e0c3ff588fd98b4f6ffd5a27bb4e2
Andrew Petersen <arobpetersen@gmail.com>
2019-07-18 15:43:36 -0400

    BuiltWith zoom/spin animation

--- src/components/BuiltWith/builtwith.jsx
+++ src/components/BuiltWith/builtwith.jsx
@@ -11,7 +11,9 @@ export class BuiltWith extends React.Component {
         return (
             <div className="builtwith">
                 <h3>Built Using:</h3>
+                <div id="reactzoom">
                 <img src={react} className="builtwith-react" alt="React" title="React"/>
+                </div>
                 <img src={reactrouter} className="builtwith-reactrouter" alt="React Router" title="React Router V4" />
                 <img src={javascript} className="builtwith-javascript" alt="JavaScript" title="JavaScript" />
                 <img src={html} className="builtwith-html" alt="HTML" title="HTML" />
