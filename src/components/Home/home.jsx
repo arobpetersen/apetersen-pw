de3b8809aee29b1b309ce91709398f90f83a7e35
Andrew Petersen <arobpetersen@gmail.com>
2019-07-24 09:56:01 -0400

    dynamic background

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,15 +1,53 @@
 import React from 'react';
 import './home.css';
+import Particles from 'react-particles-js';
+
 import { BuiltWith } from '../BuiltWith/builtwith.jsx';
 import { Intro } from '../Intro/intro.jsx';
 //import { Self } from '../SelfPicture/self.jsx';
 
+const particlesOpt = {
+    "particles": {
+        "number": {
+            "value": 160,
+            "density": {
+                "enable": false
+            }
+        },
+        "size": {
+            "value": 10,
+            "random": true
+        },
+        "move": {
+            "direction": "bottom",
+            "out_mode": "out"
+        },
+        "line_linked": {
+            "enable": false
+        }
+    },
+    "interactivity": {
+        "events": {
+            "onclick": {
+                "enable": true,
+                "mode": "remove"
+            }
+        },
+        "modes": {
+            "remove": {
+                "particles_nb": 10
+            }
+        }
+    }
+}
 class Home extends React.Component {
     render() {
         return (
             <div className="home">
+                <Particles
+                    params={particlesOpt} />
                 <Intro />
-                
+
                 <BuiltWith />
             </div>
         );
