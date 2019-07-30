44d3d9343e0959c18b50dfd55486689e1850565a
Andrew Petersen <peteraw@mail.uc.edu>
2019-07-30 17:21:06 -0400

    home view image adjustments

--- src/components/BuiltWith/builtwith.jsx
+++ src/components/BuiltWith/builtwith.jsx
@@ -11,18 +11,17 @@ import css from './img/css.png';
 export class BuiltWith extends React.Component {
     render() {
         return (
-            <div id="test">
-                <div className="builtwith">
-                    <h3>Built Using:</h3>
-                    <div id="reactzoom">
-                        <img src={react} className="builtwith-react" alt="React" title="React" />
-                    </div>
-                    <img src={reactrouter} className="builtwith-reactrouter" alt="React Router" title="React Router V4" />
-                    <img src={javascript} className="builtwith-javascript" alt="JavaScript" title="JavaScript" />
-                    <img src={html} className="builtwith-html" alt="HTML" title="HTML" />
-                    <img src={css} className="builtwith-css" alt="CSS" title="CSS" />
+
+            <div className="builtwith">
+                <h3>Built Using:</h3>
+                <div id="reactzoom">
+                    <img src={react} className="builtwith-react" alt="React" title="React" />
                 </div>
-            </div >
+                <img src={reactrouter} className="builtwith-reactrouter" alt="React Router" title="React Router V4" />
+                <img src={javascript} className="builtwith-javascript" alt="JavaScript" title="JavaScript" />
+                <img src={html} className="builtwith-html" alt="HTML" title="HTML" />
+                <img src={css} className="builtwith-css" alt="CSS" title="CSS" />
+            </div>
         );
     }
 } 
